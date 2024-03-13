154bdb9c96057b41b28067c1216d56191a4a345d
Dibakardebnath <ddibakar190@gmail.com>
2024-03-13 17:04:16 +0530

    done

--- figmaapp/src/Home.jsx
+++ figmaapp/src/Home.jsx
@@ -1,67 +1,64 @@
-import React from 'react'
-import './Home.css'
-import navbar from './Images/navbar.png'
-import madeira from './Images/madeira.png'
-import group13 from './Images/Group 13.svg'
-import image19 from './Images/image 19.png'
-import redLine from './Images/redLine.png'
-import textLine from './Images/textLine.png'
-import group12 from './Images/Group 12.png'
-import why from './Images/why-img.png'
-import virtual from './Images/virtual tour n stats.png'
-import group23 from './Images/Group 23.png'
-
-
-
-
-
-
-
-
-
-
+import React from "react";
+import "./Home.css";
+import navbar from "./Images/navbar.png";
+import madeira from "./Images/madeira.png";
+import group13 from "./Images/Group 13.svg";
+import image19 from "./Images/image 19.png";
+import redLine from "./Images/redLine.png";
+import textLine from "./Images/textLine.png";
+import group12 from "./Images/Group 12.png";
+import why from "./Images/why-img.png";
+import virtual from "./Images/virtual tour n stats.png";
+import group23 from "./Images/Group 23.png";
 
 const Home = () => {
   return (
-    <div className='main-container'>
-         
-         <div className='nav-container'>
-         <img className='nav-img' src={navbar} alt="" />
+    <div className="main-container">
+      <div className="nav-container">
+        <img className="nav-img" src={navbar} alt="" />
+      </div>
+      <div className="madeira-container">
+        <img className="madeira-img" src={madeira} alt="" />
+      </div>
 
-         </div>
-         <div className='madeira-container'>
-            <img className='madeira-img' src={madeira} alt="" />
-         </div>
+      <div className="group3">
+        <img width={"100%"} className="grp3-img" src={group13} alt="" />
+        <img
+          className="maskgroup-img"
+          src="https://static-00.iconduck.com/assets.00/youtube-icon-512x359-dyeyi1hm.png"
+          alt=""
+        />
+      </div>
 
-         <div className='group3'>
-            <img width={'100%'} className='grp3-img' src={group13} alt=''/>
-            <img className='maskgroup-img' src='https://static-00.iconduck.com/assets.00/youtube-icon-512x359-dyeyi1hm.png' alt="" />
-         </div>
-       
-       <div className='girlImg-main-container'>
-        <div className='girl-img'><img width={'100%'} src={image19} alt="" /></div>
-        <div className='girl-second-container'>
-          <img width={'100%'} src={redLine} alt="" />
-          <img width={'100%'} src={textLine} alt="" />
-          <img className='girl-container-button' width={'50%'} src={group12} alt="" />
+      <div className="girlImg-main-container">
+        <div className="girl-img">
+          <img width={"100%"} src={image19} alt="" />
         </div>
-       </div>
+        <div className="girl-second-container">
+          <img width={"100%"} src={redLine} alt="" />
+          <img width={"100%"} src={textLine} alt="" />
+          <img
+            className="girl-container-button"
+            width={"50%"}
+            src={group12}
+            alt=""
+          />
+        </div>
+      </div>
 
-       <div className='why-container'>
-        <img width={'100%'} src={why} alt="" />
-       </div>
+      <div className="why-container">
+        <img width={"100%"} src={why} alt="" />
+      </div>
 
-       <div className='white-container'>
+      <div className="white-container">
+        <img width={"100%"} src={virtual} alt="" />
+      </div>
 
-        <img width={'100%'} src={virtual} alt="" />
-
-       </div>
-      
-       <footer className='footer'>
-    <img width={'100%'} src={group23} alt="" />
-       </footer>
+      <footer className="footer">
+        <img width={"100%"} src={group23} alt="" />
+      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
